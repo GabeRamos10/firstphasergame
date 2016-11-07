58904d8be573a519770d3875a44ce390244b5464
Gabriel Ramos <gramos.sh@mysummitps.org>
2016-11-07 20:11:45 +0000

    Update

--- myGame/myGame.js
+++ myGame/myGame.js
@@ -20,15 +20,20 @@ game_state.main.prototype = {
 
     create: function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
         game.add.sprite(0, 0, 'sky');
         game.add.sprite(0, 0, 'star');
+        
         this.platforms = game.add.group();
         this.platforms.enableBody = true;
+        
         var ground = this.platforms.create(0, game.world.height - 64, 'ground');
-        ground.scale.setTo(2, 2)
-        ground.body.immovable = true
+        ground.scale.setTo(2, 2);
+        ground.body.immovable = true;
+        
         var ledge = this.platforms.create(100, 400, 'ground');
-        ledge.body.immovable = true
+        ledge.body.immovable = true;
+        
         this.player = game.add.sprite(32, game.world.height - 110, 'dude');
         game.physics.arcade.enable(this.player);
         this.player.body.bounce.y = 0.2;
@@ -36,20 +41,24 @@ game_state.main.prototype = {
         this.player.body.collideWorldBounds = true;
         this.player.animations.add('left', [0, 1, 2, 3], 10, true);
         this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-        this.cursors = game.input.keyboard.createCursorKeys
+        
+        this.cursors = game.input.keyboard.createCursorKeys;
+        
         this.stars = game.add.group();
         this.stars.enableBody = true;
+        
         for (var i = 0; i < 12; i++) {
             var star = this.stars.create(i * 70, 0, 'star');
-            this.stars = game.add.group();
-this.stars.enableBody = true;
-for (var i = 0; i < 12; i++) {
-    var star = this.stars.create(i * 70, 0, 'star');
-    star.body.gravity.y = 300;
-    star.body.bounce.y = 0.7 + Math.random() * 0.2
-  
+            star.body.gravity.y = 300;
+            star.body.bounce.y = 0.7 + Math.random() * 0.2;
         }
-}
+        
+        
+        this.score = 0;
+        this.scoreText = game.add.text(16, 16, 'score: 0', {
+            fontSize: '32px',
+            fill: '#000'
+        });
     },
 
 
@@ -57,6 +66,7 @@ for (var i = 0; i < 12; i++) {
     update: function() {
         game.physics.arcade.collide(this.player, this.platforms);
         this.cursors = game.input.keyboard.createCursorKeys();
+        
         this.player.body.velocity.x = 0;
         if (this.cursors.left.isDown) {
             this.player.body.velocity.x = -150;
@@ -77,9 +87,17 @@ for (var i = 0; i < 12; i++) {
         if (this.cursors.up.isDown && this.player.body.touching.down) {
             this.player.body.velocity.y = -350;
          }
-      game.physics.arcade.collide(this.stars, this.platforms);}
-}
-game.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
-
+      game.physics.arcade.collide(this.stars, this.platforms);
+      game.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
+    },
+    
+    
+    
+    collectStar: function(player, star){
+        star.kill();
+        this.score++;
+        this.scoreText.text = "score: " + this.score;
+    }
+};
 game.state.add('main', game_state.main);
 game.state.start('main');
